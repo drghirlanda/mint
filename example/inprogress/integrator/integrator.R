926089b93955d868a76025d89ac9002f766eed5e
Stefano Ghirlanda <drghirlanda@gmail.com>
2013-09-12 09:18:55 -0400

    Untracked a bunch of uninteresting files

--- example/inprogress/integrator/integrator.R
+++
@@ -1,6 +0,0 @@
-d <- data.frame(read.table("output/integrator.dat"))
-names(d) <- c("t","n1")
-attach(d)
-pdf( "output/integrator.pdf", height=5, width=5 )
-plot( t, n1, type="l", ylab="output", ylim=c(0,1) )
-dev.off()
