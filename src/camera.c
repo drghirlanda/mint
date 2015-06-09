d6f5d08d8ddc9d8979a79a5bf90b740e2fa37b51
Stefano Ghirlanda <drghirlanda@gmail.com>
2015-06-09 15:09:49 -0400

    Fixed bug occuring when multiple nodes access camera

--- src/camera.c
+++ src/camera.c
@@ -113,7 +113,7 @@ void mint_network_camera( struct mint_network *net, float *p ) {
   struct mint_op *op;
   float r, g, b;
   float bounds[] = {0, 1, -1};
-  uint8_t *img;
+  uint8_t *img, *pixel;
   struct timespec t1, t2;
 
   if( ! mint_camera_pid )
@@ -176,16 +176,17 @@ void mint_network_camera( struct mint_network *net, float *p ) {
 
       mint_nodes_set( n, var, 0 ); /* set node variable to zero */
 
+      pixel = img;
       for( yorig=0; yorig<height; yorig++ ) {
 	for( xorig=0; xorig<width; xorig++ ) {
 	  x = xorig * ((float)cols / width);
 	  y = yorig * ((float)rows / height);
 	  k = x + y * cols;
-	  n[var][k] += r * img[0];
-	  n[var][k] += g * img[1];
-	  n[var][k] += b * img[2];
+	  n[var][k] += r * pixel[0];
+	  n[var][k] += g * pixel[1];
+	  n[var][k] += b * pixel[2];
 	  n[var][k] /= 3*256;
-	  img += 3;
+	  pixel += 3;
 	}
       }
 
