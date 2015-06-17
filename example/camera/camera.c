5c72bd728e9413634ed2bc66155102f76afaf304
Stefano Ghirlanda <drghirlanda@gmail.com>
2015-06-17 13:33:45 -0400

    Added a few printfs

--- example/camera/camera.c
+++ example/camera/camera.c
@@ -13,15 +13,27 @@ int main( void ) {
   mint_image_init();
   mint_camera_init();
 
+  printf( "creating network... " );
+  fflush(stdout);
   file = fopen( "camera.arc", "r" );
   net = mint_network_load( file );
   fclose( file );
+  printf( "done\n" );
+  fflush(stdout);
 
+  printf( "running network... " );
+  fflush(stdout);
   for( i=0; i<1000; i++ ) {
     mint_network_operate( net );
   }
+  printf( "done\n" );
+  fflush(stdout);
 
+  printf( "deleting network... " );
+  fflush(stdout);
   mint_network_del( net );
+  printf( "done\n" );
+  fflush(stdout);
 
   return 0;
 }
